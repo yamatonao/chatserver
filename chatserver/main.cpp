634291643f264789b562bbc9409c3df8c8ffcdaa
yamatonao <yamatonao.net@gmail.com>
2016-10-25 14:20:37 +0900

    Initial Commit

---
+++ chatserver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chatserver
+//
+//  Created by yamatonao on 2016/10/25.
+//  Copyright © 2016年 yamatonao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
